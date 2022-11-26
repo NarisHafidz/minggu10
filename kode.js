651a2504fd64875ccd6f388c2d7a7dcd8ba37c59
NarisHafidz <112996214+NarisHafidz@users.noreply.github.com>
2022-11-26 13:36:34 +0700

    Add files via upload

---
+++ kode.js
@@ -0,0 +1,7 @@
+alert("Hello, ini adalah program JS eksternal!"); 
+var title = "Belajar Pemrograman Javascript"; 
+var siteName = "Belajar Kode"; 
+var url = "http://dinus.ac.id"; 
+var visitorCount = 1000;
+var site_name = "Belajar Kode"; 
+var visitor_count = 1243;
